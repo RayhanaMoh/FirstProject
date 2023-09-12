93596fba62597c52df512596e82a9a4d19997806
RayhanaMoh <144276032+RayhanaMoh@users.noreply.github.com>
2023-09-12 16:00:37 +0300

    Delete src/Rio.java

--- src/Rio.java
+++
@@ -1,8 +0,0 @@
-public class Rio {
-    public static void main(String[] args){
-
-        double[] score = {2.5, 3.1, 5.0, 1.1};
-        score[3] = score[3]+10;
-        System.out.println(score[3]);
-    }
-}
