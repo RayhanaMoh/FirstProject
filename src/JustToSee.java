604a125fb3c5eb2981bb937b40703be43bc160ef
Rayhana <rayhaan1425@gmail.com>
2023-09-12 15:50:15 +0300

    First comment

---
+++ src/JustToSee.java
@@ -0,0 +1,3 @@
+public class JustToSee {
+    System.out.println("")
+}
---
+++ src/Rio.java
@@ -0,0 +1,8 @@
+public class Rio {
+    public static void main(String[] args){
+
+        double[] score = {2.5, 3.1, 5.0, 1.1};
+        score[3] = score[3]+10;
+        System.out.println(score[3]);
+    }
+}
